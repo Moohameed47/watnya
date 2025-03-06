127e2a69c41c0e21aba5a4c0ee6c613d595b0672
MohamedEMAM388 <MohamedEmam2302301@gmail.com>
2025-03-06 06:35:16 +0200

    add controllers

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentsController
+{
+    // ROLES
+    // 1- role: student
+    // 2- role: doctor
+    // 3- role: admin
+}
