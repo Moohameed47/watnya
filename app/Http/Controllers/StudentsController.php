339017c660aae4b396f1b202eccbc240cf8ab207
Mohamed Hisham <d.mohamedtarek12@gmail.com>
2025-03-13 00:55:19 +0200

    all Gets

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentsController
@@ -10,4 +11,18 @@ class StudentsController
     // 1- role: student
     // 2- role: doctor
     // 3- role: admin
+    public function index(Request $request)
+    {
+        $query = Student::query();
+        if ($request->has('level')) {
+            $query->where('level', $request->level);
+        }
+        if ($request->has('specialization')) {
+            $query->where('specialization', $request->specialization);
+        }
+
+        $students = $query->get(['name', 'email', 'phoneNumber', 'level', 'specialization']);
+        return response()->json(['students' => $students], 200);
+    }
+
 }
--- app/Http/Controllers/SubjectsController.php
+++ app/Http/Controllers/SubjectsController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subject;
 use Illuminate\Http\Request;
 
 class SubjectsController
 {
-    //
+    // Get All Subject to be shown in the first page of subjects
+    public function index(){
+        $subjects = Subject::all(['code', 'name', 'creditHours', 'specialization', 'level', 'semester']);
+        return response()->json(['subjects' => $subjects], 200);
+    }
+
+
 }
